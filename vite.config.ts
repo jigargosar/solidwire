c4c46f7e4a23643dcff9086d77b40157cef3cab8
Jigar Gosar <jigar.gosar@gmail.com>
2026-04-24 13:59:11 +0530

    chore: switch vite-plugin-solid-devtools locator key to Ctrl

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
             "locator": {
                 "targetIDE": "vscode",
                 "componentLocation":true,
-                "key":"Alt",
+                "key":"Control",
                 "jsxLocation" : true,
             },
 
